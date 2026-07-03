73ea0e8a55800a7c5886eb2cf64ee68f5421474d
Mohankumar <mohan908053@gmail.com>
2026-07-03 13:54:02 +0530

    Add files via upload

---
+++ Student Management/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Details</h1>
+
+      <div className="card">
+        <p><b>Name :</b> Mohankumar T</p>
+        <p><b>Register No :</b> 720823104072</p>
+        <p><b>Department :</b> Electronics and Communication Engineering</p>
+        <p><b>Year :</b> Final Year</p>
+        <p><b>College :</b> XYZ Engineering College</p>
+        <p><b>Email :</b> mohankumar@gmail.com</p>
+        <p><b>Phone :</b> 9876543210</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Student Management/src/assets/student.jsx
@@ -0,0 +1,17 @@
+function Student() {
+    return (
+        <div>
+            <h2>Student Details</h2>
+
+            <p><b>Name:</b> Mohankumar T</p>
+            <p><b>Register No:</b> 720823104072</p>
+            <p><b>Department:</b> Electronics and Communication Engineering</p>
+            <p><b>Year:</b> Final Year</p>
+            <p><b>College:</b> XYZ Engineering College</p>
+            <p><b>Email:</b> mohankumar@gmail.com</p>
+            <p><b>Phone:</b> 9876543210</p>
+        </div>
+    );
+}
+
+export default Student;
